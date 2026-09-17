6f2e7334a8cd662055513fae8025d165447870e9
quanstcno2-coder <quanstcno2@gmail.com>
2026-09-17 10:15:11 +0700

    Update config.js
    Add R2 image worker

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,6 @@
 window.DDV_CONFIG = {
   SUPABASE_URL: "https://xgmkqlgpycbhbqudxqxf.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_Lw1KebuOJLL7t2Um0FKGow_aS2w7W-Q ",
+  R2_WORKER_URL: "https://ddv-image-worker.quanstcno2.workers.dev",
   DEMO_MODE: false
 };
